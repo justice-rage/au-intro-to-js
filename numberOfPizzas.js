ed293e1287ace4d5285cb7954db00168a43450ee
justicepelteir <justice.douglas@hey.com>
2022-11-25 12:44:39 -0800

    complete 4: typed orders

--- numberOfPizzas.js
+++
@@ -1,9 +0,0 @@
-function numberOfPizzas(orders) {
-    let totalPizzas = 0;
-    for(let index = 0; index < orders.length; index++) {
-        totalPizzas += orders[index].pizzas;
-    }
-    return totalPizzas;
-}
-
-module.exports = numberOfPizzas;
--- test.js
+++ test.js
@@ -1,26 +1,34 @@
-const numberOfPizzas = require('../numberOfPizzas');
 const { assert } = require('chai');
+const ORDER_TYPES = require('../orderTypes');
 
-describe('numberOfPizzas', () => {
-    describe('a single order', () => {
-        const orders = [
-            { pizzas: 3 },
-        ]
+describe('ORDER_TYPES', () => {
+    it('should have at least three keys', () => {
+        const keys = Object.keys(ORDER_TYPES);
+        assert.isAbove(keys.length, 2);
+    });
 
-        it('should return the number of pizzas', () => {
-            assert.equal(numberOfPizzas(orders), 3);
+    it('should have all keys in upper snake case', () => {
+        const keys = Object.keys(ORDER_TYPES);
+        keys.forEach((key) => {
+            assert((new RegExp(/^([A-Z]*_?)*$/)).test(key), 'the key should be in UPPER_SNAKE_CASE');
         });
     });
 
-    describe('many orders', () => {
-        const orders = [
-            { pizzas: 3 },
-            { pizzas: 5 },
-            { pizzas: 10 },
-        ]
+    it('should have a PIZZA key with a value of 0', () => {
+        assert.equal(ORDER_TYPES.PIZZA, 0);
+    });
 
-        it('should return the number of pizzas', () => {
-            assert.equal(numberOfPizzas(orders), 18);
+    it('all keys should have numerical values', () => {
+        const values = Object.values(ORDER_TYPES);
+        values.forEach((value) => {
+            assert.equal(typeof value, "number", "make sure each value is a number!");
+        });
+    });
+
+    it('no key should have the same value', () => {
+        const values = Object.values(ORDER_TYPES);
+        values.forEach((value, index) => {
+            assert(values.indexOf(value) === index, `Each value must be unique, found two ${value} values!`)
         });
     });
 });
