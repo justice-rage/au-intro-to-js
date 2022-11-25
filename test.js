c5d170ec058c545dd0ab360f666eefee144fddba
justicepelteir <justice.douglas@hey.com>
2022-11-25 12:55:13 -0800

    complete 5: add by type

--- test.js
+++ test.js
@@ -1,34 +1,30 @@
+const numberOfPizzas = require('../numberOfPizzas');
 const { assert } = require('chai');
 const ORDER_TYPES = require('../orderTypes');
 
-describe('ORDER_TYPES', () => {
-    it('should have at least three keys', () => {
-        const keys = Object.keys(ORDER_TYPES);
-        assert.isAbove(keys.length, 2);
-    });
+describe('numberOfPizzas', () => {
+    describe('just pizza', () => {
+        const orders = [
+            { pizzas: 3, type: ORDER_TYPES.PIZZA, extraCheese: true },
+            { pizzas: 5, type: ORDER_TYPES.PIZZA, extraCheese: false },
+            { pizzas: 10, type: ORDER_TYPES.PIZZA, extraCheese: true },
+        ]
 
-    it('should have all keys in upper snake case', () => {
-        const keys = Object.keys(ORDER_TYPES);
-        keys.forEach((key) => {
-            assert((new RegExp(/^([A-Z]*_?)*$/)).test(key), 'the key should be in UPPER_SNAKE_CASE');
+        it('should return the number of pizzas', () => {
+            assert.equal(numberOfPizzas(orders), 18);
         });
     });
 
-    it('should have a PIZZA key with a value of 0', () => {
-        assert.equal(ORDER_TYPES.PIZZA, 0);
-    });
+    const keys = Object.keys(ORDER_TYPES);
+    describe(keys.join(', '), () => {
+        const orders = keys.reduce((arr, key) => {
+            return arr.concat({
+                type: key,
+            });
+        }, [{ pizzas: 3, type: ORDER_TYPES.PIZZA }]);
 
-    it('all keys should have numerical values', () => {
-        const values = Object.values(ORDER_TYPES);
-        values.forEach((value) => {
-            assert.equal(typeof value, "number", "make sure each value is a number!");
-        });
-    });
-
-    it('no key should have the same value', () => {
-        const values = Object.values(ORDER_TYPES);
-        values.forEach((value, index) => {
-            assert(values.indexOf(value) === index, `Each value must be unique, found two ${value} values!`)
+        it('should return the number of pizzas', () => {
+            assert.equal(numberOfPizzas(orders), 3);
         });
     });
 });
