91168feb560fdcac64acd156826736389ad487f6
justicepelteir <justice.douglas@hey.com>
2022-11-25 12:59:33 -0800

    complete 6: object keys

--- orderTypes.js
+++
@@ -1,7 +0,0 @@
-const ORDER_TYPES = {
-    PIZZA: 0,
-    WINGS: 1,
-    BURGER: 2,
-}
-
-module.exports = ORDER_TYPES;
--- test.js
+++ test.js
@@ -1,30 +1,12 @@
-const numberOfPizzas = require('../numberOfPizzas');
+const numberOfKeys = require('../numberOfKeys');
 const { assert } = require('chai');
-const ORDER_TYPES = require('../orderTypes');
 
-describe('numberOfPizzas', () => {
-    describe('just pizza', () => {
-        const orders = [
-            { pizzas: 3, type: ORDER_TYPES.PIZZA, extraCheese: true },
-            { pizzas: 5, type: ORDER_TYPES.PIZZA, extraCheese: false },
-            { pizzas: 10, type: ORDER_TYPES.PIZZA, extraCheese: true },
-        ]
-
-        it('should return the number of pizzas', () => {
-            assert.equal(numberOfPizzas(orders), 18);
-        });
+describe('numberOfKeys', () => {
+    it('should handle an object with 1 property', () => {
+        assert.equal(numberOfKeys({ prop: 1 }), 1);
     });
 
-    const keys = Object.keys(ORDER_TYPES);
-    describe(keys.join(', '), () => {
-        const orders = keys.reduce((arr, key) => {
-            return arr.concat({
-                type: key,
-            });
-        }, [{ pizzas: 3, type: ORDER_TYPES.PIZZA }]);
-
-        it('should return the number of pizzas', () => {
-            assert.equal(numberOfPizzas(orders), 3);
-        });
+    it('should handle an object with 3 properties', () => {
+        assert.equal(numberOfKeys({ a: 1, b: 2, c: 3 }), 3);
     });
 });
