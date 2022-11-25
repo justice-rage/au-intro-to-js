825e9a445db489931c77f0d044af41991a65eca1
justicepelteir <justice.douglas@hey.com>
2022-11-25 12:27:52 -0800

    complete 2: how many pizzas

--- order.js
+++
@@ -1,7 +0,0 @@
-const order = {
-    pizzas: 4,
-    extraCheese: true,
-    deliveryInstructions: "Drop off Pizza at the front door."
-};
-
-module.exports = order;
--- test.js
+++ test.js
@@ -1,20 +1,28 @@
-const order = require('../order');
+const numberOfPizzas = require('../numberOfPizzas');
 const { assert } = require('chai');
 
 describe('order', () => {
-    it('should have a number of pizzas', () => {
-        assert(order.hasOwnProperty('pizzas'), "make sure to add pizzas to the order object!");
-        assert.equal(typeof order.pizzas, "number");
-        assert.isAbove(order.pizzas, 0);
+    describe('1 pizzas', () => {
+        const order = {
+            pizzas: 1,
+            extraCheese: true,
+            deliveryInstructions: "Round the back, please!",
+        }
+
+        it('should return the number of pizzas', () => {
+            assert.equal(numberOfPizzas(order), order.pizzas);
+        });
     });
 
-    it('should have an extraCheese boolean', () => {
-        assert(order.hasOwnProperty('extraCheese'), "make sure to add extraCheese to the order object!");
-        assert.equal(typeof order.extraCheese, "boolean");
-    });
+    describe('5 pizzas', () => {
+        const order = {
+            pizzas: 5,
+            extraCheese: false,
+            deliveryInstructions: "Call when you arrive",
+        }
 
-    it('should have a deliveryInstructions string', () => {
-        assert(order.hasOwnProperty('deliveryInstructions'), "make sure to add deliveryInstructions to the order object!");
-        assert.equal(typeof order.deliveryInstructions, "string");
+        it('should return the number of pizzas', () => {
+            assert.equal(numberOfPizzas(order), order.pizzas);
+        });
     });
 });
