7a61bec409c4b0b94afaaf760a2fd94d4cb18e6c
justicepelteir <justice.douglas@hey.com>
2022-11-25 13:23:22 -0800

    complete 1: shortest string

--- shortestString.js
+++ shortestString.js
@@ -1,5 +1,8 @@
 function shortestString(str1, str2) {
+    const str1Length = str1.length;
+    const str2Length = str2.length;
 
+    return (str1Length < str2Length? str1 : str2);
 }
 
 module.exports = shortestString;
