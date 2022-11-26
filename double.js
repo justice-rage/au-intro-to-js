5a4a2817089d72253043e89d252d015251cbd2b8
justicepelteir <justice.douglas@hey.com>
2022-11-25 18:34:51 -0800

    complete 3: and operator

--- double.js
+++
@@ -1,5 +0,0 @@
-function double(x) {
-    return (x || 0) * 2;
-}
-
-module.exports = double;
--- test.js
+++ test.js
@@ -1,17 +1,32 @@
-const double = require('../double');
+const canBreathe = require('../canBreathe');
 const { assert } = require('chai');
 
-describe('configMessage', () => {
-    describe('when there is a number passed', () => {
-        it('should double it', () => {
-            assert.equal(double(2), 4, "Did not return the double of 2!");
-            assert.equal(double(4), 8, "Did not return the double of 4!");
-            assert.equal(double(5), 10, "Did not return the double of 5!");
+describe('canBreathe', () => {
+    describe('when above water', () => {
+        it('should be able to breathe', () => {
+            assert.equal(canBreathe(false, false, true), true);
+            assert.equal(canBreathe(true, false, true), true);
+            assert.equal(canBreathe(false, true, true), true);
+            assert.equal(canBreathe(true, true, true), true);
         });
     });
-    describe('when there is no number passed', () => {
-        it('should default to zero', () => {
-            assert.equal(double(), 0, "Passing undefined for x should return 0");
+    describe('when not above water', () => {
+        describe('and not connected', () => {
+            it('should not be able to breathe', () => {
+                assert.equal(canBreathe(true, false, false), false);
+                assert.equal(canBreathe(false, false, false), false);
+            });
+        });
+        describe('and there is no oxygen', () => {
+            it('should not be able to breathe', () => {
+                assert.equal(canBreathe(false, true, false), false);
+                assert.equal(canBreathe(false, false, false), false);
+            });
+        });
+        describe('and is connected with oxygen', () => {
+            it('should be able to breathe', () => {
+                assert.equal(canBreathe(true, true, false), true);
+            });
         });
     });
 });
