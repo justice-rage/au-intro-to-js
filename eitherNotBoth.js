a2ea784efab48e066ab9e8cc567beec6771be4b7
justicepelteir <justice.douglas@hey.com>
2022-11-26 13:47:26 -0800

    complete 1: either not both

--- eitherNotBoth.js
+++ eitherNotBoth.js
@@ -1,5 +1,8 @@
 function eitherNotBoth(num) {
+    const isDivisibleBy3 = num % 3 === 0;
+    const isDivisibleBy5 = num % 5 === 0;
 
+    return ((isDivisibleBy3 || isDivisibleBy5) && !(isDivisibleBy3 && isDivisibleBy5));
 }
 
 module.exports = eitherNotBoth;
