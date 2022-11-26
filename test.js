00b67219a60f8861b06f0e38628a4baf4a7f18e8
justicepelteir <justice.douglas@hey.com>
2022-11-25 18:11:13 -0800

    begin logical operators section

--- test.js
+++ test.js
@@ -1,28 +1,5 @@
-const {assert} = require('chai');
-const playerHandScore = require('../playerHandScore');
+function willEat(hasPizza, hasDonuts, hasCookies) {
+    
+}
 
-describe('playerHandScore', () => {
-    it('should return a score of 0', () => {
-        const hand = "";
-        const result = playerHandScore(hand);
-        assert.equal(result, 0);
-    });
-
-    it('should return the total hand score', () => {
-        const hand = "J";
-        const result = playerHandScore(hand);
-        assert.equal(result, 2);
-    });
-
-    it('should return the total hand score', () => {
-        const hand = "QQ";
-        const result = playerHandScore(hand);
-        assert.equal(result, 6);
-    });
-
-    it('should return the total hand score', () => {
-        const hand = "JKQQ";
-        const result = playerHandScore(hand);
-        assert.equal(result, 12);
-    });
-});
+module.exports = willEat;
