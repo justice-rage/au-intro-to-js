72c3873169d6bb06ea16425a9984c7e930af3d85
justicepelteir <justice.douglas@hey.com>
2022-11-26 18:45:27 -0800

    complete 3: warrior

--- test.js
+++ test.js
@@ -1,26 +1,15 @@
 const { assert } = require('chai');
+const Warrior = require('../Warrior');
 const Hero = require('../Hero');
 
-describe('new Hero', () => {
-    const hero = new Hero();
+describe('new Warrior', () => {
+    const warrior = new Warrior();
+    
+    it('should have 50 health', () => {
+        assert.equal(warrior.health, 50);
+    });
 
-    describe('after taking 5 damage', () => {
-        before(() => {
-            hero.takeDamage(5);
-        });
-
-        it('should have 45 health', () => {
-            assert.equal(hero.health, 45);
-        });
-
-        describe('after taking 20 more damage', () => {
-            before(() => {
-                hero.takeDamage(20);
-            });
-
-            it('should have 25 health', () => {
-                assert.equal(hero.health, 25);
-            });
-        });
+    it('should be a hero', () => {
+        assert(warrior instanceof Hero);
     });
 });
