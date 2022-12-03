134e4fd1228ede0e811651d2e31f2b3f3224b446
justicepelteir <justice.douglas@hey.com>
2022-12-03 12:03:51 -0800

    complete 6: is unique

--- group.js
+++
@@ -1,9 +0,0 @@
-function group(foods) {
-    return foods.reduce((accumulator, currentValue) => {
-        accumulator[currentValue.type] = accumulator[currentValue.type] || [];
-        accumulator[currentValue.type].push(currentValue.food);
-        return accumulator;
-    }, {});
-}
-
-module.exports = group;
