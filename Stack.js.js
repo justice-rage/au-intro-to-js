dbfd859ec600ab8cf102dab5023663a7b29998cb
justicepelteir <justice.douglas@hey.com>
2022-12-03 13:17:09 -0800

    complete 2: overflow & underflow

--- Stack.js.js
+++ Stack.js.js
@@ -5,16 +5,22 @@ class Stack {
         this.items = [];
     }
     push(item) {
+        if (this.items.length === MAX_STACK_SIZE) {
+            throw new Error("No more room to add items");
+        }
         this.items.push(item);
     }
     pop() {
+        if (this.items.length === 0) {
+            throw new Error("No more items to remove");
+        }
         return this.items.pop();
     }
     isEmpty() {
         
     }
     peek() {
-        
+
     }
 }
 
