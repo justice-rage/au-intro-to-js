8087bf0ae6adce2ce8186f3febf63a96aa425c4d
justicepelteir <justice.douglas@hey.com>
2022-12-03 15:55:51 -0800

    complete 4: operations manager

---
+++ operationsManager.js
@@ -0,0 +1,28 @@
+const Stack = require('./Stack');
+
+class OperationManager {
+    constructor() {
+        this.operations = new Stack();
+        this.undos = new Stack();
+    }
+
+    addOperation(operation) {
+        this.operations.push(operation);
+    }
+
+    undo() {
+        
+    }
+
+    redo() {
+        
+    }
+
+    redoAll() {
+        
+    }
+}
+
+
+
+module.exports = OperationManager;
