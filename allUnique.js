176c675c7df543589c3761af3097a5b69dec3c58
justicepelteir <justice.douglas@hey.com>
2022-12-03 12:09:38 -0800

    begin recursion section

--- allUnique.js
+++
@@ -1,8 +0,0 @@
-function allUnique(numbers) {
-    return numbers.reduce((accumulator, currentValue, index) => {
-        if (!accumulator) return false;
-        return numbers.indexOf(currentValue) === index;
-    }, true);
-}
-
-module.exports = allUnique;
---
+++ factorial.js
@@ -0,0 +1,5 @@
+function factorial(n) {
+    
+}
+
+module.exports = factorial;
