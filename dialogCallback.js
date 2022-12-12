6027a1b8b76baabb3b7ce00b140a139bdc51e542
justicepelteir <justice.douglas@hey.com>
2022-12-11 17:40:32 -0800

    complete 4: dialog callbacks

--- dialogCallback.js
+++ dialogCallback.js
@@ -1,11 +1,20 @@
 class Dialog {
+    constructor(){
+        this.callbacks = [];
+    }
     onclose(callbackFunction) {
         // store callback
-        this.callback = callbackFunction;
+        // this.callback = callbackFunction;
+        this.callbacks.push(callbackFunction);
     }
 
     close() {
         // invoke the callback
-        this.callback();
+        // this.callback();
+        let index = -1;
+        while(++index < this.callbacks.length)
+        this.callbacks[index]();
     }
 }
+
+module.exports = Dialog;
--- testDialogCallback.js
+++ testDialogCallback.js
@@ -2,13 +2,23 @@ const Dialog = require('../dialogCallback');
 const { assert } = require('chai');
 
 describe('Dialog', () => {
-    it('should allow me to register a callback', () => {
+    it('should allow registering of multiple callbacks', () => {
         const dialog = new Dialog();
-        let called = false;
+        let calledA = 0;
+        let calledB = 0;
+        let calledC = 0;
         dialog.onClose(() => {
-            called = true;
+            calledA++;
+        });
+        dialog.onClose(() => {
+            calledB++;
+        });
+        dialog.onClose(() => {
+            calledC++;
         });
         dialog.close();
-        assert(called, "Callback was not called after close");
+        assert.equal(calledA, 1, "First Callback was not called once after close");
+        assert.equal(calledB, 1, "Second Callback was not called once after close");
+        assert.equal(calledC, 1, "Third Callback was not called once after close");
     });
 });
